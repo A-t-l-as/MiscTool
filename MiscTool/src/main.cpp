1a9a5bf3afa87398e4e450982188fe7389b643c5
A-t-l-as <fulljestprp@gmail.com>
2026-09-03 08:40:55 +0200

    Update: main

--- MiscTool/src/main.cpp
+++ MiscTool/src/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    MiscToolApp my_wpk_tool_app;
-    my_wpk_tool_app.Run(argc, argv);
+    MiscToolApp my_misc_tool_app;
+    my_misc_tool_app.Run(argc, argv);
 
 	exit(EXIT_SUCCESS);
 }
